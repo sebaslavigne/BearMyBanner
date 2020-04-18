172a6e00736c3a932316ea8ef7c126cd745dac0f
BlackHawkPL <arturo2028@gmail.com>
2020-04-18 10:34:32 +0200

    Added tests project

---
+++ BearMyBannerTests/BannerAssignmentTests.cs
@@ -0,0 +1,21 @@
+﻿using BearMyBanner;
+using Xunit;
+
+namespace BearMyBannerTests
+{
+    public class BannerAssignmentTests
+    {
+        private readonly BannerAssignmentController _sut;
+
+        public BannerAssignmentTests()
+        {
+            _sut = new BannerAssignmentController();
+        }
+
+        [Fact]
+        public void TestCreation()
+        {
+            Assert.NotNull(_sut);
+        }
+    }
+}
