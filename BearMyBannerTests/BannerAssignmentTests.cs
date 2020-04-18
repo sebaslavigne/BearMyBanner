bfb16417cef26648c8a5ccac4958930be5b1ded4
BlackHawkPL <arturo2028@gmail.com>
2020-04-18 15:00:47 +0200

    Refactored tests

--- BearMyBannerTests/BannerAssignmentTests.cs
+++ BearMyBannerTests/BannerAssignmentTests.cs
@@ -25,43 +25,6 @@ namespace BearMyBannerTests
             _settings.SetDefaults();
         }
 
-        private ICharacter GetBasicInfantry()
-        {
-            var mock = new Mock<ICharacter>();
-            mock.Setup(m => m.Occupation)
-                .Returns(CharacterOccupation.Soldier);
-            mock.Setup(m => m.Type)
-                .Returns(TroopSpecialization.Infantry);
-            mock.Setup(m => m.Tier)
-                .Returns(5);
-
-            return mock.Object;
-        }
-
-        private ICharacter GetArcher()
-        {
-            var mock = new Mock<ICharacter>();
-            mock.Setup(m => m.Occupation)
-                .Returns(CharacterOccupation.Soldier);
-            mock.Setup(m => m.Type)
-                .Returns(TroopSpecialization.Archer);
-            mock.Setup(m => m.Tier)
-                .Returns(5);
-
-            return mock.Object;
-        }
-
-        private IAgent AgentFor(ICharacter character, string party = "testParty")
-        {
-            var mock = new Mock<IAgent>();
-            mock.Setup(m => m.Character)
-                .Returns(character);
-            mock.Setup(m => m.PartyName)
-                .Returns(party);
-
-            return mock.Object;
-        }
-
         [Fact]
         public void TestCreation()
         {
@@ -71,11 +34,14 @@ namespace BearMyBannerTests
         [Fact]
         public void TestBasicBattleWithDefaultSettings()
         {
-            var basicInfantry = GetBasicInfantry();
-            var characters = Enumerable.Repeat(basicInfantry, 1).ToList();
-            _sut.FilterAllowedBearerTypes(characters, false);
+            var basicInfantry = CharacterFactory.GetBasicInfantry();
+            _sut.FilterAllowedBearerTypes(new [] {basicInfantry}, false);
 
-            foreach (var agent in Enumerable.Repeat(AgentFor(basicInfantry), 21))
+            var party = new PartyBuilder("testParty")
+                .AddTroops(basicInfantry, 21)
+                .Build();
+
+            foreach (var agent in party)
             {
                 _sut.ProcessAgentOnBuild(agent, BattleType.FieldBattle);
             }
@@ -86,14 +52,16 @@ namespace BearMyBannerTests
         [Fact]
         public void TestMixedTroops()
         {
-            var basicInfantry = GetBasicInfantry();
-            var archer = GetArcher();
+            var basicInfantry = CharacterFactory.GetBasicInfantry();
+            var archer = CharacterFactory.GetArcher();
             _sut.FilterAllowedBearerTypes(new List<ICharacter>(new []{archer, basicInfantry}), false);
 
-            var agents = Enumerable.Repeat(AgentFor(basicInfantry), 21)
-                .Concat(Enumerable.Repeat(AgentFor(archer), 14));
+            var party = new PartyBuilder("testParty")
+                .AddTroops(basicInfantry, 21)
+                .AddTroops(archer, 14)
+                .Build();
 
-            foreach (var agent in agents)
+            foreach (var agent in party)
             {
                 _sut.ProcessAgentOnBuild(agent, BattleType.FieldBattle);
             }
@@ -104,22 +72,32 @@ namespace BearMyBannerTests
         [Fact]
         public void TestTwoParties()
         {
-            var basicInfantry = GetBasicInfantry();
-            var archer = GetArcher();
+            var basicInfantry = CharacterFactory.GetBasicInfantry();
+            var archer = CharacterFactory.GetArcher();
             _sut.FilterAllowedBearerTypes(new List<ICharacter>(new[] { archer, basicInfantry }), false);
 
-            var agentsForFirstParty = Enumerable.Repeat(AgentFor(basicInfantry, "firstParty"), 21)
-                .Concat(Enumerable.Repeat(AgentFor(archer, "firstParty"), 14));
+            var firstParty = new PartyBuilder("firstParty")
+                .AddTroops(basicInfantry, 21)
+                .AddTroops(archer, 14)
+                .Build();
 
-            var agentsForSecondParty = Enumerable.Repeat(AgentFor(basicInfantry, "secondParty"), 14)
-                .Concat(Enumerable.Repeat(AgentFor(archer, "secondParty"), 7));
+            var secondParty = new PartyBuilder("secondParty")
+                .AddTroops(basicInfantry, 14)
+                .AddTroops(archer, 14)
+                .Build();
 
-            foreach (var agent in agentsForFirstParty.Concat(agentsForSecondParty))
+            foreach (var agent in firstParty.Concat(secondParty))
             {
                 _sut.ProcessAgentOnBuild(agent, BattleType.FieldBattle);
             }
 
             Assert.Equal(5, _sut.AgentsThatShouldReceiveBanners.Count());
+        }
+
+        [Fact]
+        public void TestWithLowTierInfantry()
+        {
+
         }
     }
 }
