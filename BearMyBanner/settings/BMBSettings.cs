c8432c0a94a3f40b4ce4b3423056d02e07a8b45c
Sebas Lavigne <sebastianlavigne@gmail.com>
2020-04-24 17:01:05 +0200

    Reordered settings properties for future serializations

--- BearMyBanner/settings/BMBSettings.cs
+++ BearMyBanner/settings/BMBSettings.cs
@@ -14,7 +14,6 @@ namespace BearMyBanner.Settings
     {
         public static IBMBSettings Instance => GetInstance();
 
-
         private static IBMBSettings _settings;
 
         private static IBMBSettings GetInstance()
@@ -30,13 +29,24 @@ namespace BearMyBanner.Settings
         {
         }
 
+        public bool AllowSieges { get; set; }
+        public bool SiegeAttackersUseBanners { get; set; }
+        public bool SiegeDefendersUseBanners { get; set; }
+        public bool AllowHideouts { get; set; }
+        public bool HideoutAttackersUseBanners { get; set; }
+        public bool HideoutBanditsUseBanners { get; set; }
+        public bool TournamentBanners { get; set; }
+        public bool TournamentThemes { get; set; }
+        public bool TournamentBannersInShields { get; set; }
+        public int BearerToTroopRatio { get; set; }
+        public bool UseTroopSpecs { get; set; }
         public bool AllowSoldiers { get; set; }
         public bool AllowCaravanGuards { get; set; }
         public bool AllowMercenaries { get; set; }
         public bool AllowBandits { get; set; }
         public bool AllowInfantry { get; set; }
-        public bool AllowMounted { get; set; }
         public bool AllowRanged { get; set; }
+        public bool AllowMounted { get; set; }
         public bool AllowMountedRanged { get; set; }
         public bool FilterTiers { get; set; }
         public bool AllowTier1 { get; set; }
@@ -49,18 +59,8 @@ namespace BearMyBanner.Settings
         public bool AllowPlayer { get; set; }
         public bool AllowCompanions { get; set; }
         public bool AllowNobles { get; set; }
-        public int BearerToTroopRatio { get; set; }
-        public bool UseTroopSpecs { get; set; }
-        public bool AllowSieges { get; set; }
-        public bool SiegeAttackersUseBanners { get; set; }
-        public bool SiegeDefendersUseBanners { get; set; }
-        public bool AllowHideouts { get; set; }
-        public bool HideoutAttackersUseBanners { get; set; }
-        public bool HideoutBanditsUseBanners { get; set; }
         public bool ShowMessages { get; set; }
         public bool WhiteMessages { get; set; }
-        public bool TournamentBanners { get; set; }
-        public bool TournamentThemes { get; set; }
-        public bool TournamentBannersInShields { get; set; }
+
     }
 }
